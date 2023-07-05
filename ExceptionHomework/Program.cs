61cba64794098390cc50fa92e2f281f3fc6d763b
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:51:44 +0200

    add app

---
+++ ExceptionHomework/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+int[] ages = new int[] { 1, 3, 5, 7, 9 };
+
+try
+{
+    for (int i = 0; i <= ages.Length; i++)
+    {
+        Console.WriteLine(ages[i]);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    throw new Exception("This is a catched exception", ex);
+}
